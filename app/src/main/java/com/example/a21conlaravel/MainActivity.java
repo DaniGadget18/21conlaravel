5bd231d85817d3724927b28446c86bdb66ced0fc
aplicaciones3 <dani_181298@hotmail.com>
2019-03-07 11:25:33 -0600

    Acabado

--- app/src/main/java/com/example/a21conlaravel/MainActivity.java
+++ app/src/main/java/com/example/a21conlaravel/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -15,6 +17,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Method;
@@ -22,11 +25,17 @@ import java.lang.reflect.Method;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView rv;
+    ImageView img1, img2, img3, img4;
+    int sumador;
+    TextView sumar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sumar = findViewById(R.id.suma);
+        img1 = findViewById(R.id.imagen1);
     }
 
     public void solicitud(View view) {
@@ -34,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
         JsonObjectRequest js = new JsonObjectRequest(Request.Method.GET, "http://nuevo.rnrsiilge-org.mx/baraja/numero",
                 null, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONObject response) {
-                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+            public void onResponse(JSONObject response)  {
+                try {
+                    Toast.makeText(MainActivity.this, response.getString("numero"), Toast.LENGTH_SHORT).show();
+                    int numero = response.getInt("numero");
+                    sumador+=numero;
+                    Toast.makeText(MainActivity.this, String.valueOf(sumador), Toast.LENGTH_SHORT).show();
 
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
